9f349ab531c6783868a30d66b36ebc444763fc64
Rahul Zore <zore.r@husky.neu.edu>
2019-05-14 22:21:06 -0400

    MergeSort

---
+++ src/com/sorting/mergesort/MergeSort.java
@@ -0,0 +1,37 @@
+package com.sorting.mergesort;
+
+public class MergeSort {
+
+	private static boolean less(Comparable a, Comparable b) {
+		return a.compareTo(b) < 0;
+	}
+	
+	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+		for(int k=lo; k<=hi; k++) {
+			aux[k] = a[k];
+		}
+		
+		int i=lo, j=mid+1;
+		
+		for(int k=lo; k<=hi; k++) {
+			if(i>mid) a[k] = aux[j++];
+			else if(j>hi) a[k] = aux[i++];
+			else if(less(aux[j], aux[i])) a[k] = aux[j++];
+			else a[k] = aux[i++];
+		}
+	}
+	
+	private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+		if(hi<=lo) return;
+		int mid = lo + (hi-lo)/2;
+		sort(a, aux, lo, mid);
+		sort(a, aux, mid+1, hi);
+		merge(a, aux, lo, mid, hi);
+	}
+	
+	public static void sort(Comparable[] a) {
+		int N = a.length;
+		Comparable[] aux = new Comparable[N];
+		sort(a, aux, 0, N-1);
+	}
+}
